9f860b8c5afc473069d02bc978c9b6144d563bb9
Thomas <thomasorbe2004@gmail.com>
2024-10-15 00:38:48 -0500

    Correciones Mapas

--- app/src/main/java/com/example/taller2icm/MapsActivity2.kt
+++ app/src/main/java/com/example/taller2icm/MapsActivity2.kt
@@ -1,9 +1,15 @@
 package com.example.taller2icm
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.location.Geocoder
 import android.location.Location
+import android.location.Address
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -19,15 +25,18 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.MapStyleOptions
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import org.json.JSONArray
+import org.json.JSONObject
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
+import java.io.IOException
 import java.io.Writer
 import java.util.Date
 import java.util.Locale
@@ -37,12 +46,13 @@ import kotlin.math.roundToInt
 import kotlin.math.sin
 import kotlin.math.sqrt
 
-class MapsActivity2 : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity2 : AppCompatActivity(), OnMapReadyCallback, SensorEventListener {
 
     companion object {
         const val REQUEST_LOCATION_PERMISSION = 1
         const val RADIUS_OF_EARTH_KM = 6.371
         const val MIN_DISTANCE_METERS = 30 // Mínimo desplazamiento en metros
+        const val LUMINOSITY_THRESHOLD = 40 // Límite para cambiar entre modo claro y oscuro
     }
 
     private lateinit var mMap: GoogleMap
@@ -50,17 +60,28 @@ class MapsActivity2 : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mFusedLocationClient: FusedLocationProviderClient
     private lateinit var mLocationRequest: LocationRequest
     private lateinit var mLocationCallback: LocationCallback
-
+    private lateinit var sensorManager: SensorManager
+    private lateinit var geocoder: Geocoder
+    private var lightSensor: Sensor? = null
     private var lastLocation: Location? = null
     private var currentMarker: Marker? = null
+    private var isLocationUpdateEnabled = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMaps2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Inicializar servicios de ubicación
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         mLocationRequest = createLocationRequest()
+
+        // Inicializar el sensor de luz
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+        // Inicializae el geocodificador
+        geocoder = Geocoder(this, Locale.getDefault())
 
         // Obtener el fragmento del mapa
         val mapFragment = supportFragmentManager
@@ -68,13 +89,23 @@ class MapsActivity2 : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
 
         // Configurar el campo de texto para buscar direcciones
-        val editTextAddress = findViewById<EditText>(R.id.texto)
-        editTextAddress.setOnEditorActionListener { _, actionId, _ ->
+        binding.texto.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                val address = editTextAddress.text.toString()
-                searchLocationByAddress(address)
+                searchLocation()
             }
             true
+        }
+
+        // Inicializar el callback de ubicación
+        mLocationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    lastLocation = location
+                    updateMarker(location)
+                    writeJSONObject(location)
+                }
+            }
         }
     }
 
@@ -159,83 +190,129 @@ class MapsActivity2 : AppCompatActivity(), OnMapReadyCallback {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
+            ) == PackageManager.PERMISSION_GRANTED && isLocationUpdateEnabled
         ) {
             mFusedLocationClient.requestLocationUpdates(
                 mLocationRequest,
                 mLocationCallback,
                 null
             )
-        } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                REQUEST_LOCATION_PERMISSION
-            )
         }
     }
 
-    // Función para buscar una dirección usando Geocoder
-    private fun searchLocationByAddress(address: String) {
-        val geocoder = Geocoder(this, Locale.getDefault())
-        val addresses = geocoder.getFromLocationName(address, 1)
-        if (addresses != null && addresses.isNotEmpty()) {
-            val location = addresses[0]
-            val latLng = LatLng(location.latitude, location.longitude)
-            mMap.addMarker(MarkerOptions().position(latLng).title(address))
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
-        } else {
-            Toast.makeText(this, "Dirección no encontrada", Toast.LENGTH_SHORT).show()
+    // Busca la ubicación en el mapa
+    private fun searchLocation() {
+
+        val address = binding.texto.toString()
+
+        try {
+            val addressList = geocoder.getFromLocationName(address, 1)
+            if (addressList != null && addressList.isNotEmpty()) {
+                val addressFound = addressList[0]
+                val latLng = LatLng(addressFound.latitude, addressFound.longitude)
+                addMarkerWithAddress(latLng)  // Agrega un marcador con la dirección
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
+            } else {
+                Toast.makeText(this, "No se encontró la dirección", Toast.LENGTH_LONG).show()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(this, "Error de geocodificación", Toast.LENGTH_LONG).show()
         }
+    }
+
+    // Agrega un marcador con la dirección
+    private fun addMarkerWithAddress(latLng: LatLng) {
+        try {
+            val addresses: List<Address> = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1) as List<Address>
+            if (addresses.isNotEmpty()) {
+                val address = addresses[0]
+                val addressText = address.getAddressLine(0) ?: "Dirección no válida"
+                mMap.addMarker(MarkerOptions().position(latLng).title(addressText))
+                Toast.makeText(this, "Marcador agregado: $addressText", Toast.LENGTH_SHORT).show()
+            } else {
+                mMap.addMarker(MarkerOptions().position(latLng).title("Marcador sin dirección"))
+                Toast.makeText(this, "No se encontró dirección para esta ubicación", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            mMap.addMarker(MarkerOptions().position(latLng).title("Marcador sin dirección"))
+            Toast.makeText(this, "Error al obtener dirección", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun distance(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Int {
+        val dLat = Math.toRadians(lat2 - lat1)
+        val dLon = Math.toRadians(lon2 - lon1)
+        val a = sin(dLat / 2) * sin(dLat / 2) +
+                cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2)) *
+                sin(dLon / 2) * sin(dLon / 2)
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        return (RADIUS_OF_EARTH_KM * c * 1000).roundToInt() // Convertir a metros
     }
 
     private fun writeJSONObject(location: Location) {
-        val localizaciones = readJSONArrayFromFile("locations.json")
-
-        localizaciones.put(
-            MyLocation(
-                Date(System.currentTimeMillis()),
-                location.latitude,
-                location.longitude
-            ).toJSON()
-        )
-
-        val filename = "locations.json"
         try {
-            val file = File(baseContext.getExternalFilesDir(null), filename)
-            Log.i("LOCATION", "Ubicacion de archivo: $file")
-            val output: Writer = BufferedWriter(FileWriter(file))
-            output.write(localizaciones.toString())
-            output.close()
-
-            Toast.makeText(applicationContext, "Ubicación guardada", Toast.LENGTH_LONG).show()
+            val jsonObject = JSONObject().apply {
+                put("provider", location.provider)
+                put("latitude", location.latitude)
+                put("longitude", location.longitude)
+                put("altitude", location.altitude)
+                put("accuracy", location.accuracy)
+                put("time", Date(location.time))
+            }
+            val jsonArray = JSONArray().apply {
+                put(jsonObject)
+            }
+            val file = File(getExternalFilesDir(null), "ubicacion.json")
+            val writer: Writer = BufferedWriter(FileWriter(file))
+            writer.use {
+                it.write(jsonArray.toString())
+            }
+            Toast.makeText(this, "Ubicación guardada en archivo", Toast.LENGTH_SHORT).show()
         } catch (e: Exception) {
-            Log.e("LOCATION", "Error al guardar la ubicación", e)
+            Log.e("JSON", "Error al escribir JSON", e)
         }
     }
 
-    private fun readJSONArrayFromFile(fileName: String): JSONArray {
-        val file = File(baseContext.getExternalFilesDir(null), fileName)
-        if (!file.exists()) {
-            Log.i("LOCATION", "Archivo no encontrado: $file")
-            return JSONArray()
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+            } else {
+                Toast.makeText(this, "Permiso de ubicación denegado", Toast.LENGTH_SHORT).show()
+            }
         }
-        val jsonString = file.readText()
-        return JSONArray(jsonString)
     }
 
-    // Calcular distancia entre dos puntos
-    fun distance(lat1: Double, long1: Double, lat2: Double, long2: Double): Double {
-        val latDistance = Math.toRadians(lat1 - lat2)
-        val lngDistance = Math.toRadians(long1 - long2)
-        val a = (sin(latDistance / 2) * sin(latDistance / 2)
-                + cos(Math.toRadians(lat1)) * cos(Math.toRadians(lat2))
-                * sin(lngDistance / 2) * sin(lngDistance / 2))
-        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
-        val distance = RADIUS_OF_EARTH_KM * c * 1000 // Convert to meters
-        return (distance * 100.0).roundToInt() / 100.0
+   /*override fun onResume() {
+        super.onResume()
+        lightSensor?.let {
+            sensorManager.registerListener(this, it, SensorManager.SENSOR_DELAY_NORMAL)
+        }
+        if(isLocationUpdateEnabled){
+            startLocationUpdates()
+        }
+    }
+*/
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event?.sensor?.type == Sensor.TYPE_LIGHT) {
+            val lightValue = event.values[0]
+            if (lightValue < LUMINOSITY_THRESHOLD) {
+                mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style_dark))
+            } else {
+                mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style_standard))
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        TODO("Not yet implemented")
     }
 }
